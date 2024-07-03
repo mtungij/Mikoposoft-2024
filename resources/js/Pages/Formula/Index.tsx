49c93f87f693806c4bca6cba83851dbc2d2f30e8
alkadoHs <alkadosichone@gmail.com>
2024-07-03 22:43:23 +0300

    ....

--- resources/js/Pages/Formula/Index.tsx
+++ resources/js/Pages/Formula/Index.tsx
@@ -19,48 +19,48 @@ import EditFormula from "./actions/EditFormula";
 
 const Index = ({ auth, formulas }: PageProps<{ formulas: Formula[] }>) => {
   return (
-    <Authenticated user={auth.user}>
-            <Head title="Loan Product" />
+      <Authenticated user={auth.user}>
+          <Head title="Loan Product" />
 
-            <section>
-                <div>
-                    <h2 className="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight first:mt-0">
-                        Interest formular
-                    </h2>
-                </div>
-                <div className="w-full grid md:grid-cols-2 items-center gap-4 pb-5">
-                    <Input
-                        type="search"
-                        name="search"
-                        className="max-w-sm"
-                        placeholder="Search..."
-                    />
+          <section className="bg-white p-5 rounded-md md:shadow-lg">
+              <div>
+                  <h2 className="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight first:mt-0">
+                      Interest formular
+                  </h2>
+              </div>
+              <div className="w-full grid md:grid-cols-2 items-center gap-4 pb-5">
+                  <Input
+                      type="search"
+                      name="search"
+                      className="max-w-sm"
+                      placeholder="Search..."
+                  />
 
-                    <div className="flex md:justify-end">
-                        <CreateFormula />
-                    </div>
-                </div>
-                <div className="border border-gray-200 rounded-md">
-                    <Table>
-                        <TableHeader>
-                            <TableRow>
-                                <TableHead>#</TableHead>
-                                <TableHead>Name</TableHead>
-                            </TableRow>
-                        </TableHeader>
-                        <TableBody>
-                            {formulas.map((formula, index) => (
-                                <TableRow>
-                                    <TableCell>{index + 1}</TableCell>
-                                    <TableCell>{formula.name}</TableCell>
-                                </TableRow>
-                            ))}
-                        </TableBody>
-                    </Table>
-                </div>
-            </section>
-        </Authenticated>
-  )
+                  <div className="flex md:justify-end">
+                      <CreateFormula />
+                  </div>
+              </div>
+              <div className="border border-gray-200 rounded-md">
+                  <Table>
+                      <TableHeader>
+                          <TableRow>
+                              <TableHead>#</TableHead>
+                              <TableHead>Name</TableHead>
+                          </TableRow>
+                      </TableHeader>
+                      <TableBody>
+                          {formulas.map((formula, index) => (
+                              <TableRow>
+                                  <TableCell>{index + 1}</TableCell>
+                                  <TableCell>{formula.name}</TableCell>
+                              </TableRow>
+                          ))}
+                      </TableBody>
+                  </Table>
+              </div>
+          </section>
+      </Authenticated>
+  );
 }
 
 export default Index
