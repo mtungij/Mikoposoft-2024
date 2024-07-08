863792e7ad86046461fc53610e3fb4a076471a9b
mtungij <jamesmtungi2013@gmail.com>
2024-07-08 13:17:38 +0300

    minor

--- resources/js/components/Sidebar.tsx
+++ resources/js/components/Sidebar.tsx
@@ -93,13 +93,6 @@ const Sidebar = () => {
                     </AccordionContent>
                 </AccordionItem>
             </Accordion>
-            <Link
-                href="#"
-                className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
-            >
-                <Users className="h-4 w-4" />
-                Customers
-            </Link>
 
             <Accordion type="single" collapsible className="w-full px-3 py-2">
                 <AccordionItem value="item-1" className="w-full">
